816de4df3aba2064da3acdb12e4b7d6ae52aff62
Sira Argia <si.ecenk@gmail.com>
2019-05-25 20:14:57 +0700

    masonry

--- source/js/app.js
+++ source/js/app.js
@@ -11,13 +11,3 @@ $(document).ready(function() {
   });
 });
 
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
-
-// init Masonry after all images have loaded
-var $grid = $('.grid').imagesLoaded( function() {
-  $grid.masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer'
-  }); 
-});
